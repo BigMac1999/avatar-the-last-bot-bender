090a56ae814d27c787c0b89cba1d8e202928c10f
Bis <bismarck.perez99@gmail.com>
2025-07-29 01:48:55 -0400

    feat: Implementing code for future pages (currently drawn out)
    Implemented the info and roster pages which will be the first to be developed. The pages have been drawn out, i will just first go to the game-engine and get the API calls / infrastructure changes necessary for some of the features i want to be able to call from the website and from the discord bot as well since development on that is starting soon.

---
+++ services/website/src/app/info/page.tsx
@@ -0,0 +1,8 @@
+export default function Info() {
+    return (
+        <div>
+            <h1>Information Page</h1>
+            <p>This is the info page of the ATLA Bot.</p>
+        </div>
+    );
+}
---
+++ services/website/src/app/roster/page.tsx
@@ -0,0 +1,8 @@
+export default function Roster() {
+    return (
+        <div>
+            <h1>Roster Page</h1>
+            <p>This is the roster page of the ATLA Bot.</p>
+        </div>
+    );
+}
